8ae00faca818b8031f69348edc143a0c58b4ae6f
Matheus Novais <matheusnovaisz@gmail.com>
2025-08-04 21:24:55 -0300

    refactor: ajusta estilizacao no component Orientacao

--- frontend/src/components/Orientacao/Orientacao.tsx
+++ frontend/src/components/Orientacao/Orientacao.tsx
@@ -11,51 +11,51 @@ import { useOrientacao } from "@/hooks/useOrientacao";
 import { getEventEditionIdStorage } from "@/context/AuthProvider/util";
 
 export default function Orientacao() {
-  const { postOrientacao, putOrientacao, getOrientacoes, orientacoes } =
-    useOrientacao();
+	const { postOrientacao, putOrientacao, getOrientacoes, orientacoes } =
+		useOrientacao();
 
-  const [content, setContent] = useState(orientacoes?.summary || "");
+	const [content, setContent] = useState(orientacoes?.summary || "");
 
-  const handleEditOrientacao = () => {
-    const idOrientacao = orientacoes?.id;
-    const eventEditionId = getEventEditionIdStorage();
+	const handleEditOrientacao = () => {
+		const idOrientacao = orientacoes?.id;
+		const eventEditionId = getEventEditionIdStorage();
 
-    if (idOrientacao) {
-      putOrientacao(idOrientacao, {
-        eventEditionId: eventEditionId ?? "",
-        summary: content,
-      });
-    } else {
-      postOrientacao({
-        eventEditionId: eventEditionId ?? "",
-        summary: content,
-      });
-    }
-  };
+		if (idOrientacao) {
+			putOrientacao(idOrientacao, {
+				eventEditionId: eventEditionId ?? "",
+				summary: content,
+			});
+		} else {
+			postOrientacao({
+				eventEditionId: eventEditionId ?? "",
+				summary: content,
+			});
+		}
+	};
 
-  useEffect(() => {
-    getOrientacoes();
-  }, []);
+	useEffect(() => {
+		getOrientacoes();
+	}, []);
 
-  useEffect(() => {
-    setContent(orientacoes?.summary || "");
-  }, [orientacoes?.summary]);
+	useEffect(() => {
+		setContent(orientacoes?.summary || "");
+	}, [orientacoes?.summary]);
 
-  return (
-    <div id="Orientacao">
-      <div className="text-white text-center display-4 fs-1 fw-bold orientacao-titulo">
-        Orientações
-      </div>
+	return (
+		<div id='Orientacao'>
+			<div className='text-white text-center display-4 fs-1 fw-bold orientacao-titulo mb-4'>
+				Orientações
+			</div>
 
-      <HtmlEditorComponent
-        content={content}
-        onChange={(newValue) => setContent(newValue)}
-        handleEditField={handleEditOrientacao}
-      />
+			<HtmlEditorComponent
+				content={content}
+				onChange={(newValue) => setContent(newValue)}
+				handleEditField={handleEditOrientacao}
+			/>
 
-      <Link className={"orientacao-link"} href="/orientacoes">
-        Ver todas as orientações
-      </Link>
-    </div>
-  );
+			<Link className={"orientacao-link"} href='/orientacoes'>
+				Ver todas as orientações
+			</Link>
+		</div>
+	);
 }
