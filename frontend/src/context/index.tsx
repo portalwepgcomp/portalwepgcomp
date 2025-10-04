2540fb304d983c43cc67cb7da64556d795fa43d4
Lucas Júlio <lucasjuliosouza.06@gmail.com>
2025-10-04 02:11:14 -0300

    refactor: Atualiza importações para utilizar hooks de contexto

--- frontend/src/context/index.tsx
+++ frontend/src/context/index.tsx
@@ -1,16 +1,16 @@
 "use client";
 import { ComposeProviders } from "@/components/ComposeProviders";
-import { UserProvider } from "./user";
-import { SessionProvider } from "./session";
-import { EvaluationProvider } from "./evaluation";
-import { OrientacaoProvider } from "./orientacoes";
-import { CommitterProvider } from "./commiteeMember";
-import { EdicaoProvider } from "./edicao";
-import { PresentationProvider } from "./presentation";
-import { SubmissionProvider } from "./submission";
-import { ActiveEditionProvider } from "./activeEdition";
-import { SubmissionFileProvider } from "./submissionFile";
-import { PremiacaoProvider } from "./premiacao";
+import { UserProvider } from "@/hooks/useUsers";
+import { SessionProvider } from "@/hooks/useSession";
+import { EvaluationProvider } from "@/hooks/useEvaluation";
+import { OrientacaoProvider } from "@/hooks/useOrientacao";
+import { CommitterProvider } from "@/hooks/useCommittee";
+import { EdicaoProvider } from "@/hooks/useEdicao";
+import { PresentationProvider } from "@/hooks/usePresentation";
+import { SubmissionProvider } from "@/hooks/useSubmission";
+import { ActiveEditionProvider } from "@/hooks/useActiveEdition";
+import { SubmissionFileProvider } from "@/hooks/useSubmissionFile";
+import { PremiacaoProvider } from "@/hooks/usePremiacao";
 
 interface ProvidersProps {
   children: React.ReactNode;
