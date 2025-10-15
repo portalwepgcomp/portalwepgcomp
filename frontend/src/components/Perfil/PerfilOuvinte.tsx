9faad511e8514ac406a5c14f9768f8af7f6536bf
Lucas Júlio <lucasjuliosouza.06@gmail.com>
2025-10-15 01:42:26 -0300

    atualiza textos nos menus de perfil (#58)

--- frontend/src/components/Perfil/PerfilOuvinte.tsx
+++ frontend/src/components/Perfil/PerfilOuvinte.tsx
@@ -8,64 +8,60 @@ import Link from "next/link";
 import "./style.scss";
 
 export default function PerfilOuvinte() {
-    const { logout } = useContext(AuthContext);
-    const { Edicao } = useEdicao();
-    const { showAlert } = useSweetAlert();
-    const { downloadCertificate } = useCertificate();
+  const { logout } = useContext(AuthContext);
+  const { Edicao } = useEdicao();
+  const { showAlert } = useSweetAlert();
+  const { downloadCertificate } = useCertificate();
 
-    const certificateDownload = async () => {
-        const response = await downloadCertificate(Edicao?.id || "");
+  const certificateDownload = async () => {
+    const response = await downloadCertificate(Edicao?.id || "");
 
-        if (response === 200) {
-            showAlert({
-                icon: "success",
-                title: "Download feito com sucesso!",
-                timer: 3000,
-                showConfirmButton: false,
-            });
-            return
-        }
-        showAlert({
-            icon: "error",
-            title: response,
-            timer: 3000,
-            showConfirmButton: false,
-        });
-    };
+    if (response === 200) {
+      showAlert({
+        icon: "success",
+        title: "Download feito com sucesso!",
+        timer: 3000,
+        showConfirmButton: false,
+      });
+      return;
+    }
+    showAlert({
+      icon: "error",
+      title: response,
+      timer: 3000,
+      showConfirmButton: false,
+    });
+  };
 
-    return (
-        <li className="dropdown">
-            <button
-                className="btn dropdown-toggle border-0"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false">
-                <i className="bi bi-list fs-3"></i>
-            </button>
-            <ul className="dropdown-menu dropdown-menu-end border-3 border-light">
-                <li>
-                    <button
-                        className="dropdown-item"
-                        onClick={certificateDownload}>
-                        Emitir Certificado
-                    </button>
-                </li>
-                <li>
-                    <Link className="dropdown-item" href="/favoritos">
-                        Favoritos
-                    </Link>
-                </li>
-                <li>
-                    <li>
-                        <Link
-                            className="dropdown-item"
-                            href="/home"
-                            onClick={logout}>
-                            Sair
-                        </Link>
-                    </li>
-                </li>
-            </ul>
+  return (
+    <li className="dropdown">
+      <button
+        className="btn dropdown-toggle border-0"
+        type="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        <i className="bi bi-list fs-3"></i>
+      </button>
+      <ul className="dropdown-menu dropdown-menu-end border-3 border-light">
+        <li>
+          <button className="dropdown-item" onClick={certificateDownload}>
+            Certificado
+          </button>
         </li>
-    );
+        <li>
+          <Link className="dropdown-item" href="/favoritos">
+            Favoritos
+          </Link>
+        </li>
+        <li>
+          <li>
+            <Link className="dropdown-item" href="/home" onClick={logout}>
+              Sair
+            </Link>
+          </li>
+        </li>
+      </ul>
+    </li>
+  );
 }
