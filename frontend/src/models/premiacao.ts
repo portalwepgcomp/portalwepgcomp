e9c1ca7ed15c07f3abdb44610e6446487282f57e
unknown <bruunaaa04@gmail.com>
2025-10-27 19:49:21 -0300

    feat: adicionando ranking e votacao no menu de avaliadores

--- frontend/src/models/premiacao.ts
+++ frontend/src/models/premiacao.ts
@@ -1,52 +1,50 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 type PremiacaoCategoriaProps = {
-    categoria: string;
-    searchValue: string;
-    premiacoes: Premiacoes[];
-    avaliadores: AuthorOrEvaluator[];
+  categoria: string;
+  searchValue: string;
+  premiacoes: Premiacoes[];
+  avaliadores: AuthorOrEvaluator[];
 };
 
 interface AuthorOrEvaluator {
-    id: string;
-    name: string;
-    email: string;
-    registrationNumber: string;
-    photoFilePath: string;
-    profile: Record<string, unknown>;
-    level: Record<string, unknown>;
-    isActive?: boolean;
+  id: string;
+  name: string;
+  email: string;
+  votes?: number;
+  registrationNumber: string;
+  photoFilePath: string;
+  profile: Record<string, unknown>;
+  level: Record<string, unknown>;
+  isActive?: boolean;
 }
 
-interface AvaliadorParams {   
-  eventEditionId: string,
+interface AvaliadorParams {
+  eventEditionId: string;
   panelists: {
-    userId: string
-  }[]
+    userId: string;
+  }[];
 }
 
-interface PanelistsParams{
-    id: string,
-    name: string,
-    email: string,
-    registrationNumber: string,
-    profile: Record<string, unknown>,
-    level: Record<string, unknown>,
-    isActive?: boolean,
-    createdAt: string,
-    updatedAt: string,
-    isVerified?: boolean
+interface PanelistsParams {
+  id: string;
+  name: string;
+  email: string;
+  registrationNumber: string;
+  profile: Record<string, unknown>;
+  level: Record<string, unknown>;
+  isActive?: boolean;
+  createdAt: string;
+  updatedAt: string;
+  isVerified?: boolean;
 }
 
-  
 interface Premiacoes {
-    id: string;
-    presentationBlockId: string;
-    positionWithinBlock: number;
-    status: Record<string, unknown>;
-    publicAverageScore: number;
-    evaluatorsAverageScore: number;
-    submission: Submission;
+  id: string;
+  presentationBlockId: string;
+  positionWithinBlock: number;
+  status: Record<string, unknown>;
+  publicAverageScore: number;
+  evaluatorsAverageScore: number;
+  submission: Submission;
 }
-  
-  
