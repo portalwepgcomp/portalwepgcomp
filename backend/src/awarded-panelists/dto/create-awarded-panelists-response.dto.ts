863fdc877eead3c28fd4971bf78e2691521bfad8
unknown <bruunaaa04@gmail.com>
2025-10-27 19:28:12 -0300

    feat: adicionando funcao de voto

--- backend/src/awarded-panelists/dto/create-awarded-panelists-response.dto.ts
+++ backend/src/awarded-panelists/dto/create-awarded-panelists-response.dto.ts
@@ -1,15 +1,12 @@
 export class CreateAwardedPanelistsResponseDto {
   addedPanelists: string[];
-  removedPanelists: string[];
   maintainedPanelists: string[];
 
   constructor(params: {
     addedPanelists: string[];
-    removedPanelists: string[];
     maintainedPanelists: string[];
   }) {
     this.addedPanelists = params.addedPanelists;
-    this.removedPanelists = params.removedPanelists;
     this.maintainedPanelists = params.maintainedPanelists;
   }
 }
